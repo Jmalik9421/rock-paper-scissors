ef91899b2310e20faaba1562b28565e8dca22146
Jawad Malik <jmalik94@outlook.com>
2023-10-14 16:26:46 +0100

    game function defined

--- script.js
+++ script.js
@@ -3,59 +3,30 @@ function getComputerChoice() {
     return computerChoice[Math.floor(Math.random() * 3)];
 }
 
-/*
-rock
-paper
-scissors
-
-Possibilites:
-rock vs rock = draw 
-                            rock vs paper = rock loses
-                            rock vs scissors = rock wins
-
-                            paper vs rock = paper wins
-paper vs paper = draw
-                            paper vs scissors = paper loses
-
-                            scissors vs rock = rock wins
-                            scissors vs paper = scissors wins
-scissors vs scissors = scissors draws 
-
-
-draws:
-rock vs rock
-paper vs paper
-scissors vs scissors
-
-wins:
-rock vs scissors = rock wins
-scissors vs paper = scissors wins
-paper vs rock = paper wins
-
-losses:
-rock vs paper = rock loses
-paper vs scissors = paper loses
-scissors vs rock = rock loses
-*/
-
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) { // define draw
+    if (playerSelection === computerSelection) {
         console.log("Draw");
     } else if (
         (playerSelection === "rock" && computerSelection === "scissors")
         || (playerSelection === "paper" && computerSelection === "rock")
         || (playerSelection === "scissors" && computerSelection == "paper")
         ) {
-            return "Player wins"
+            return "Player wins";
     } else if (
         (computerSelection === "rock" && playerSelection === "scissors")
         || (computerSelection === "paper" && playerSelection === "rock")
         || (computerSelection === "scissors" && playerSelection === "paper")
     ) {
-      return "Computer wins"
+      return "Computer wins";
     } 
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game(rounds) {
+    for (let currentRound = 1; currentRound <= rounds; currentRound++) {
+        const playerSelection = prompt("Enter your choice");
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game(5);
