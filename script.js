fb784313195ee65c38d676babb9aaf41187c04be
Jawad Malik <jmalik94@outlook.com>
2023-10-23 08:58:44 +0100

    buttons added for player input code reformatted to use arrow notation, logic to only play 5 games removed

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
-function getComputerChoice() {
-    const computerChoice = ["rock", "paper", "scissors"];
-    return computerChoice[Math.floor(Math.random() * 3)];
-}
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
 
-function playRound(playerSelection, computerSelection) {
+const getComputerChoice = () => {
+    const computerChoice = ['rock', 'paper', 'scissors'];
+    return computerChoice[Math.floor(Math.random() * computerChoice.length)];
+};
+
+const playRound = (playerSelection, computerSelection) => {
+    let result;
     if (playerSelection === computerSelection) {
-        console.log("Draw");
+        result = 'Draw';
     } else if (
-        (playerSelection === "rock" && computerSelection === "scissors")
-        || (playerSelection === "paper" && computerSelection === "rock")
-        || (playerSelection === "scissors" && computerSelection == "paper")
+        (playerSelection === 'rock' && computerSelection === 'scissors')
+        || (playerSelection === 'paper' && computerSelection === 'rock')
+        || (playerSelection === 'scissors' && computerSelection == 'paper')
         ) {
-            return "Player wins";
+            result = 'Player wins';
     } else if (
-        (computerSelection === "rock" && playerSelection === "scissors")
-        || (computerSelection === "paper" && playerSelection === "rock")
-        || (computerSelection === "scissors" && playerSelection === "paper")
+        (computerSelection === 'rock' && playerSelection === 'scissors')
+        || (computerSelection === 'paper' && playerSelection === 'rock')
+        || (computerSelection === 'scissors' && playerSelection === 'paper')
     ) {
-      return "Computer wins";
+      result = 'Computer wins';
     } 
-}
+    
+    return result;
+};
 
-function game(rounds) {
-    for (let currentRound = 1; currentRound <= rounds; currentRound++) {
-        const playerSelection = prompt("Enter your choice");
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
+rock.addEventListener('click', () => {
+    const playerSelection = 'rock';
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+})
 
-game(5);
+paper.addEventListener('click', () => {
+    const playerSelection = 'paper';
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+})
+
+scissors.addEventListener('click', () => {
+    const playerSelection = 'scissors';
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+})
